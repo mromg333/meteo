1111d03958b150f22094ba89ed3b5b76685b4c71
mromg333 <xatdimitris4@gmail.com>
2025-04-28 22:48:13 +0300

    Update Primary Price Analysis.py

--- Primary Price Analysis.py
+++ Primary Price Analysis.py
@@ -4,17 +4,14 @@ import sys
 
 sys.stdout.reconfigure(encoding='utf-8')
 
-csv_file = "2022_2025.csv"
+csv_file = "z1Score.csv"
 df = pd.read_csv(csv_file, sep=',', encoding='utf-8', low_memory=False)
-
 
 df['Temp_Out[degC]'] = pd.to_numeric(df['Temp_Out[degC]'], errors='coerce')
 df['Hum_Out[%]'] = pd.to_numeric(df['Hum_Out[%]'], errors='coerce')
 df['Wind_Speed[m/s]'] = pd.to_numeric(df['Wind_Speed[m/s]'], errors='coerce')
 
-
 fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(14, 12), sharex=True)
-
 
 colors = ['tomato', 'dodgerblue', 'seagreen']
 metrics = {
@@ -23,20 +20,24 @@ metrics = {
     'Wind_Speed[m/s]': ('Ταχύτητα Άνεμου (m/s)', colors[2]),
 }
 
-
 for idx, (column, (ylabel, color)) in enumerate(metrics.items()):
     ax = axes[idx]
     for year in [2022, 2023, 2024]:
         year_data = df[df['Year'] == year]
-        monthly_avg = year_data.groupby('Month')[column].mean()
-        ax.plot(monthly_avg.index, monthly_avg.values, label=str(year), color=color, linestyle=['-', '--', ':'][year % 3])
+        filtered_data = year_data[(year_data[column].notna()) & (year_data[column] != 0)]
+        
+        if not filtered_data.empty:
+            monthly_avg = filtered_data.groupby('Month')[column].mean()
+            ax.plot(monthly_avg.index, monthly_avg.values, label=str(year), color=colors[year-2022], linestyle=['-', '--', ':'][year % 3])
     
     ax.set_ylabel(ylabel, fontsize=12)
-    ax.legend()
+    ax.legend(title='Έτος')
     ax.grid(True)
+
 plt.suptitle('Ανάλυση Πρωτογενή Τιμών')
-axes[-1].set_xlabel('Μεσος Ορος Ανά Μήνα', fontsize=12)
+axes[-1].set_xlabel('Μέσος Όρος Ανά Μήνα', fontsize=12)
 plt.xticks(range(1, 13))
-plt.tight_layout()
-#plt.savefig('data.png')
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+fig.subplots_adjust(hspace=0.01)
+# plt.savefig('data.png')
 plt.show()
